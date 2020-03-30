ad8d88d11c568638c9fe2ffca6f2811b52ec4511
unknown <angeyuangeyu@gmail.com>
2020-03-30 17:00:02 -0500

    it is employee time

--- src/App.js
+++ src/App.js
@@ -1,14 +1,48 @@
-import React from "react";
+import React, {Component} from "react";
 import Nav from "./components/Nav";
 import Content from "./components/Content";
 import Container from "./components/Container";
+import API from './utils/API';
 
+//can also do React.Component w/o importing compoentn
+class App extends Component {
+  state = {
+    results: [],
+    loading: false,
+  }
 
-function App() {
-  return (
+  componentDidMount() {
+      this.setState({loading: true})
+      API.getEmployees().then(response => {
+        console.log(response.data.results)
+        this.setState({ 
+          results: response.data.results,
+          loading: false,
+        })
+      })
+  }
 
-        <Content />
-  );
+  render (){
+    return (
+      <>
+        <Nav />
+        
+        { this.state.loading ? (
+          <p>Loading...</p>
+        ) : (
+          <Container>
+            <Content employees = {this.state.results}/>
+          </Container>
+        ) }
+        
+      </>    
+    );
+  }
 }
+
+
+
+//render method returns JSX
+
 
 export default App;
--- src/components/Content.js
+++ src/components/Content.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './content.css';
-import getEmployee from './API'
+import getEmployee from '../utils/API'
 
 function Content(props) { console.log(getEmployee.results)
   return (
@@ -8,19 +8,22 @@ function Content(props) { console.log(getEmployee.results)
     <table className= "col-8">
       <thead>
         <tr>
-          <th scope="col">{props.library}</th>
+          <th scope="col">pic</th>
           <th scope="col">Name</th>
           <th scope="col">Phone#</th>
           <th scope="col">Email</th>
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td scope="row">call in pic?</td>
-          <td>name</td>
-          <td>Phone</td>
-          <td>email</td>
-        </tr>
+        {props.employees.map(employee => (
+          <tr key = {employee.id.value}>
+              <td><img src={employee.picture.thumbnail}/></td>
+              <td>{employee.name.first} {employee.name.last}</td>
+              <td>{employee.phone}</td>
+              <td>{employee.email}</td>
+          </tr>
+
+        ))}
       </tbody>
     </table>
   );
